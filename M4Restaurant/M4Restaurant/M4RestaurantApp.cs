7b8d81cd241d1200d4dd9002a6af238c2fc16420
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 12:56:35 +0100

    In process

---
+++ M4Restaurant/M4Restaurant/M4RestaurantApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace M4Restaurant
+{
+    class M4RestaurantApp
+    {
+        static void Main(string[] args)
+        {
+            Menu M4Restaurant = new Menu();
+
+            M4Restaurant.ShowMenu(); // Enseñamos el menú al cliente
+            M4Restaurant.AskWhatClientsWant(); // Le pedimos que elija los platos
+            M4Restaurant.CalcularLaDolorosa(); // Calculamos la cuenta.
+        }
+    }
+}
